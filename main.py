eac68b84fb9decf5e7e524daceb07ac88b542963
mikegoolsby <55331311+mikegoolsby@users.noreply.github.com>
2021-08-27 14:42:57 -0400

    removes precanned dicts

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
 from datetime import date
 
-reps_pd_hours = {
-    'ALLANA GILGEOUS' : 7.5,
-    'SALLY ROBERTS' : 5.0,
-    'MATEO CURCURU' : 3.5,
-    'DARIUS L. WOODS' : 3.0,
-    'TALI WEG' : 3.0,
-    'LAKSHMI BABUREDDY' : 2.5,
-    'BRIAN LI' : 2.5,
-    'ANINA VOLCY' : 2.5,
-    'LIAM FITZGERALD' : 2.0,
-    'JONATHAN SPIELMAN' : 2.0,
-    'BRIAN PARKER' : 1.0,
-    'MELISSA TYSON' : 1.0,
-    'THERESE CHERIAN' : 0.0,
-    'ABHIT PAL' : 0.0
-}
-
-reps_output = {
-    'ALLANA GILGEOUS' : 11.0,
-    'SALLY ROBERTS' : 4.0,
-    'MATEO CURCURU' : 2.0,
-    'DARIUS L. WOODS' : 1.0,
-    'TALI WEG' : 1.0,
-    'LAKSHMI BABUREDDY' : 10.0,
-    'BRIAN LI' : 3.0,
-    'ANINA VOLCY' : 1.0,
-    'LIAM FITZGERALD' : 1.0,
-    'JONATHAN SPIELMAN' : 4.0,
-    'BRIAN PARKER' : 1.0,
-    'MELISSA TYSON' : 2.0,
-    'THERESE CHERIAN' : 0.0,
-    'ABHIT PAL' : 0.0
-}
-
 def show_inefficient(hours, output):
 
     today = date.today()
